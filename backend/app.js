eb68f4d9a2fb782a7bb157af43ae1f6cc537616d
rifacoder <jstrfaheem065@gmail.com>
2020-03-17 21:50:03 +0530

    Login resolver,

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,17 @@ app.use(
   graphqlHttp({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
-    graphiql: true
+    graphiql: true,
+    customFormatErrorFn(err) {
+      if (!err.originalError) {
+        return err;
+      }
+
+      const data = err.originalError.data;
+      const message = err.message;
+      const code = err.originalError.code || 500;
+      return { message: message, status: code, data: data };
+    }
   })
 );
 
--- backend/graphql/resolver.js
+++ backend/graphql/resolver.js
@@ -1,7 +1,33 @@
+const bcrypt = require("bcrypt");
+
+const jwt = require("jsonwebtoken");
+
 const User = require("../model/user");
 
 module.exports = {
   login: async ({ email, password }) => {
-    return { token: "token", userId: "userId" };
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      const error = new Error("user doesn't exist!");
+      error.code = 401;
+      throw error;
+    }
+
+    const isEqual = await bcrypt.compare(user.password, password);
+
+    if (!isEqual) {
+      const error = new Error("not authenticated!");
+      error.code = 401;
+      throw error;
+    }
+
+    const token = jwt.sign(
+      { userId: user._id.toString(), email: user.email },
+      process.env.secret_word,
+      { expiresIn: "1h" }
+    );
+
+    return { token: token, userId: user._id.toString() };
   }
 };
