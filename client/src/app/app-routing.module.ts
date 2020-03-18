33e18fee160e67564dd64875a36411146162b8aa
rifacoder <jstrfaheem065@gmail.com>
2020-03-18 19:56:59 +0530

    Auth guard,

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./auth/login/login.component";
 import { SignupComponent } from "./auth/signup/signup.component";
+import { HomeComponent } from "./user/home/home.component";
+import { AuthGuard } from "./auth/auth.guard";
 
 const routes: Routes = [
   {
@@ -16,11 +18,17 @@ const routes: Routes = [
   {
     path: "auth/signup",
     component: SignupComponent
+  },
+  {
+    path: "user/home",
+    component: HomeComponent,
+    canActivate: [AuthGuard]
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule {}
