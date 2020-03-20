f3d32fef231e34c8442595489d84c2ce0d2056f2
rifacoder <jstrfaheem065@gmail.com>
2020-03-20 21:09:14 +0530

    Showing members,

---
+++ client/src/app/user/member/member.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ParamMap, ActivatedRoute } from "@angular/router";
+import { UserService } from "../user.service";
+import { AuthService } from "src/app/auth/auth.service";
+import { Subscription } from "rxjs";
+import { User } from "../user.model";
+
+@Component({
+  selector: "app-member",
+  templateUrl: "./member.component.html",
+  styleUrls: ["./member.component.css"]
+})
+export class MemberComponent implements OnInit, OnDestroy {
+  ngOnDestroy(): void {
+    this.usersSub.unsubscribe();
+  }
+  private usersSub: Subscription;
+
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+    private route: ActivatedRoute
+  ) {}
+
+  email: string;
+  name: string;
+  userId: string;
+  imageUrl = "";
+  address: string;
+  arrears: number;
+  users: User[] = [];
+
+  ngOnInit(): void {
+    this.userService.getAllUsers();
+    this.usersSub = this.userService
+      .getUserUpdateListener()
+      .subscribe(users => {
+        this.users = users;
+      });
+  }
+}
