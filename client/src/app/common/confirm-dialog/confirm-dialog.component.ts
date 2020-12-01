36ee2550c1932172589d419596822223cc1329c3
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2020-12-01 14:42:45 +0530

    UI/UX enhancement,

--- client/src/app/common/confirm-dialog/confirm-dialog.component.ts
+++ client/src/app/common/confirm-dialog/confirm-dialog.component.ts
@@ -3,6 +3,7 @@ import { MAT_DIALOG_DATA } from "@angular/material/dialog";
 
 @Component({
   templateUrl: "confirm-dialog.component.html",
+  styleUrls: ["./confirm-dialog.component.css"],
 })
 export class ConfirmDialogComponent {
   constructor(@Inject(MAT_DIALOG_DATA) public data: { msg: string }) {}
